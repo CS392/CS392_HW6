3bb971ba0e6d971e0862b4f43ce327642b8908c5
SciencePotato <hchan7235@bths.edu>
2023-10-13 21:09:31 -0400

    Add project files.

---
+++ 14.1-Console/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Replace with your own file path
+string fileContent = File.ReadAllText(@"C:\Users\houch\Downloads\random.txt");
+
+List<int> res = new List<int>();
+foreach (string line in fileContent.Split("\n"))
+{
+    res.Add(int.Parse(line));
+}
+
+// Remove all Negative number
+res.RemoveAll(x => x < 0);
+res = res.FindAll(x => x >= 1 && x <= 10);    
+
+for (int i = 0; i < res.Count; i++)
+{
+    Console.WriteLine(res[i]);
+}
+
---
+++ 14.1-WinForm/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _14._1_WinForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string fileContent = File.ReadAllText($@"{textBox1.Text}");
+
+            List<int> res = new List<int>();
+            foreach (string line in fileContent.Split('\n'))
+            {
+                res.Add(int.Parse(line));
+            }
+
+            // Remove all Negative number
+            res.RemoveAll(x => x < 0);
+            res = res.FindAll(x => x >= 1 && x <= 10);
+
+            listBox1.DataSource = res;
+        }
+    }
+}
